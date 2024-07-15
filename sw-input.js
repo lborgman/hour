3a9a335369a2fea4cb8ee7de725fc6e1d8f1aa8f
lborgman <lennart.borgman@gmail.com>
2024-07-15 15:14:50 +0200

    Just test update again

--- sw-input.js
+++ sw-input.js
@@ -1,4 +1,4 @@
-const SW_VERSION = "0.9.001";
+const SW_VERSION = "0.9.002";
 /*
     This is a boilerplate for a simple PWA service worker.
     When you want to create a new version of the service worker then

24ec80531f94cf48adb4e73cf40857bd2c3664a3
lborgman <lennart.borgman@gmail.com>
2024-07-16 05:01:29 +0200

    Bump version to test install

--- sw-input.js
+++ sw-input.js
@@ -1,4 +1,4 @@
-const SW_VERSION = "0.9.037";
+const SW_VERSION = "0.9.038";
 /*
     This is a boilerplate for a simple PWA service worker.
     When you want to create a new version of the service worker then

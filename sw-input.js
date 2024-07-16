510b54c612d72bcbd60b1a80548fa89bc60d44d9
lborgman <lennart.borgman@gmail.com>
2024-07-16 20:24:40 +0200

    Try to fix the height of the explanation div

--- sw-input.js
+++ sw-input.js
@@ -1,4 +1,4 @@
-const SW_VERSION = "0.9.072";
+const SW_VERSION = "0.9.075";
 /*
     This is a boilerplate for a simple PWA service worker.
     When you want to create a new version of the service worker then

f25afcef731a52a7851613ddb6e5b44d0ab346c9
lborgman <lennart.borgman@gmail.com>
2024-07-23 11:45:15 +0200

    Bump version number

--- sw-input.js
+++ sw-input.js
@@ -1,5 +1,5 @@
 // See pwa.js for documentation
-const SW_VERSION = "0.9.229";
+const SW_VERSION = "0.9.230";
 
 const logStyle = "background:blue; color:white; padding:2px; border-radius:2px;";
 const logStrongStyle = logStyle + " font-size:18px;";

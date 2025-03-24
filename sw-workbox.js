627fd13583d5bfd680724ba89d9719d641fac43c
lborgman <lennart.borgman@gmail.com>
2025-03-24 11:26:56 +0100

    Relative scope

--- sw-workbox.js
+++ sw-workbox.js
@@ -1,5 +1,5 @@
 // See pwa.js for documentation
-const SW_VERSION = "0.9.289";
+const SW_VERSION = "0.9.291";
 
 // https://www.npmjs.com/package/workbox-sw
 const urlWorkbox = 'https://storage.googleapis.com/workbox-cdn/releases/7.1.0/workbox-sw.js';
@@ -21,7 +21,7 @@ logStrongConsole(`Service worker SW_VERSION=${SW_VERSION}`);
 
 
 importScripts(urlWorkbox);
-workbox.precaching.precacheAndRoute([{"revision":"b0a78421af923baebb18919f8701abde","url":"hour.html"},{"revision":"104b7da4d74d59b641ffb764822d1850","url":"hour.svg"},{"revision":"6bfe278097e33ae7bdf4bddd1fdc2155","url":"hour2.svg"},{"revision":"bd8902a5e61cc2f23f87b2e8c713e08e","url":"manifest.json"},{"revision":"f93701e07741c718b1b84a319ed4ff65","url":"pwa-not-cached.js"},{"revision":"80e72f7af45a37a85211ab403845c228","url":"pwa.js"},{"revision":"60e7a81b2c2c6bf66b43c97a5b1f0ce5","url":"sw-reset.js"},{"revision":"9733a10260c76a60abfe450622e5201a","url":"workbox-config.js"}]);
+workbox.precaching.precacheAndRoute([{"revision":"b0a78421af923baebb18919f8701abde","url":"hour.html"},{"revision":"104b7da4d74d59b641ffb764822d1850","url":"hour.svg"},{"revision":"6bfe278097e33ae7bdf4bddd1fdc2155","url":"hour2.svg"},{"revision":"f93701e07741c718b1b84a319ed4ff65","url":"pwa-not-cached.js"},{"revision":"80e72f7af45a37a85211ab403845c228","url":"pwa.js"},{"revision":"60e7a81b2c2c6bf66b43c97a5b1f0ce5","url":"sw-reset.js"},{"revision":"5ab96a535ccf032be938484962af2632","url":"workbox-config.js"}]);
 
 
 

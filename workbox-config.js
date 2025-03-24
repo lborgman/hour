b5735a37cc28d43846193ae719ffcfd22d4b7abc
lborgman <lennart.borgman@gmail.com>
2025-03-24 11:11:05 +0100

    Relative paths for scope etc

--- workbox-config.js
+++ workbox-config.js
@@ -1,7 +1,8 @@
 module.exports = {
 	globDirectory: '.',
 	globPatterns: [
-		'**/*.{html,svg,js,json}'
+		// '**/*.{html,svg,js,json}'
+		'**/*.{html,svg,js}'
 	],
 	swSrc: 'sw-input.js',
 	swDest: 'sw-workbox.js',
